6b071c4dfc3b976ed30597d518947d754de119ba
George Ollis <George.Ollis@outlook.com>
2026-05-11 08:58:27 +0100

    feat: add redirect for About Me page and remove Certifications link from 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://georgeollis.github.io',
   output: 'static',
+  redirects: {
+    '/about-me': '/me',
+  },
 });
